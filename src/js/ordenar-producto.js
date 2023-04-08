f70bfc7c9c75658a1fff1cba26e7d4cc1dabe0b2
Alonso Bonilla <luis.labp06@gmail.com>
2023-04-07 19:06:15 -0500

    Organizando las carpetas del pry

---
+++ src/js/ordenar-producto.js
@@ -0,0 +1,10 @@
+import { evento } from "./tmp.js";
+let objExport = {};
+
+document.addEventListener('DOMContentLoaded', () => {
+    evento();
+})
+
+
+export { objExport };
+// export { nombre_producto, descripcion_producto, precio_producto, img_producto };
