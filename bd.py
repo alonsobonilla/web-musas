8821dae1e7b686b9aef1ef9ec68cd75c85d9cbe6
Alonso Bonilla <luis.labp06@gmail.com>
2023-05-01 13:39:58 -0500

    Usando un archivo de configuración para la conexión a la bd

--- bd.py
+++ bd.py
@@ -1,6 +1,15 @@
-import pymysql
+import pymysql, configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+host = config.get('database', 'host')
+port = int(config.get('database', 'port'))
+db = config.get('database', 'database_name')
+username = config.get('database','username')
+password = config.get('database', 'password')
 
 def obtener_conexion():
-    return pymysql.connect(host='127.0.0.1', port=3306, user='root', password='root', db='desarrollo_web')
+    return pymysql.connect(host=host, port=port, user=username, password=password, db=db)
 
 
