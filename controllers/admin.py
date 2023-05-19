b3490d65b1657d66e4a472406fff7d28f97e2f10
Alonso Bonilla <luis.labp06@gmail.com>
2023-05-18 21:58:26 -0500

    Creando un blueprint para productos

--- controllers/admin.py
+++ controllers/admin.py
@@ -6,49 +6,8 @@ admin = Blueprint('admin', __name__, url_prefix='/admin')
 @admin.route("/")
 def home():
     user_id = session.get("admin.auth")
-
     if user_id is None:
         return render_template("admin/login.html")
     else:
         productos = Producto.obtener_productos()
-        return render_template("admin/index.html", productos=productos, usuario = user_id)
-
-@admin.route("/agregar_producto")
-def formulario_agregar_producto():
-    return render_template("admin/agregar_producto.html")
-
-
-@admin.route("/guardar_producto", methods=["POST"])
-def guardar_producto():
-    nombre = request.form["nombre"]
-    descripcion = request.form["descripcion"]
-    precio = request.form["precio"]
-    existencias = request.form["existencias"]
-    Producto.insertar_producto( nombre, descripcion, precio, existencias)
-    # De cualquier modo, y si todo fue bien, redireccionar
-    return redirect("/")
-
-@admin.route("/eliminar_producto", methods=["POST"])
-def eliminar_producto():
-    Producto.eliminar_producto(request.form["id"])
-    return redirect("/")
-
-
-@admin.route("/formulario_editar_producto/<int:id>")
-def editar_producto(id):
-    # Obtener el disco por ID
-    producto = Producto.obtener_producto_por_id(id)
-    return render_template("admin/editar_producto.html", producto=producto)
-
-
-@admin.route("/actualizar_producto", methods=["POST"])
-def actualizar_producto():
-    id = request.form["id"]
-    nombre = request.form["nombre"]
-    descripcion = request.form["descripcion"]
-    precio = request.form["precio"]
-    existencias = request.form["existencias"]
-    Producto.actualizar_producto(nombre, descripcion, precio, existencias, id)
-    return redirect("/")
-
-
+        return render_template("admin/productos/index.html", productos=productos, usuario = user_id)
---
+++ controllers/admin_productos.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, redirect, request, session, url_for
+from controllers.admin import admin
+from model.Producto import Producto
+from model.CategoriaProducto import CategoriaProducto
+
+productos = Blueprint("productos", __name__)
+
+@productos.route("/agregar_producto")
+def formulario_agregar():
+    nombreCategorias = CategoriaProducto.obtener_categorias()
+    return render_template("admin/productos/agregar_producto.html", categorias = nombreCategorias)
+
+@productos.route("/guardar_producto", methods=["POST"])
+def guardar():
+    nombre = request.form["nombre"]
+    descripcion = request.form["descripcion"]
+    precio = request.form["precio"]
+    existencias = request.form["existencias"]
+    Producto.insertar_producto( nombre, descripcion, precio, existencias)
+    # De cualquier modo, y si todo fue bien, redireccionar
+    return redirect("/")
+
+@productos.route("/eliminar_producto", methods=["POST"])
+def eliminar():
+    Producto.eliminar_producto(request.form["id"])
+    return redirect("/")
+
+
+@productos.route("/formulario_editar_producto/<int:id>")
+def editar(id):
+    # Obtener el disco por ID
+    producto = Producto.obtener_producto_por_id(id)
+    return render_template("admin/productos/editar_producto.html", producto=producto)
+
+
+@productos.route("/actualizar_producto", methods=["POST"])
+def actualizar():
+    id = request.form["id"]
+    nombre = request.form["nombre"]
+    descripcion = request.form["descripcion"]
+    precio = request.form["precio"]
+    existencias = request.form["existencias"]
+    Producto.actualizar_producto(nombre, descripcion, precio, existencias, id)
+    return redirect("/")
+
+
+@productos.before_request
+def verificacion_usuario_logueado():
+    user_id = session.get("admin.auth")
+
+    if user_id is None:
+        return redirect(url_for("admin.home"))
