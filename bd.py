1901cdb58a17fad6d603632ddbab197836e63d88
Alonso Bonilla <luis.labp06@gmail.com>
2023-05-26 22:26:47 -0500

    Agregando metodo ssl para azure

--- bd.py
+++ bd.py
@@ -1,10 +1,7 @@
-import pymysql, configparser
+import pymysql
 from cfg import*
 
 def obtener_conexion():
-    if port == '' :
-        return pymysql.connect(host=host, user=username, password=password, db=db)
-    else:
-        return pymysql.connect(host=host, port= port, user=username, password=password, db=db)
+    return pymysql.connect(host=host, port= port, user=username, password=password, db=db, ssl={'ssl': {'sslmode': 'require', 'ssl_ca': 'DigiCertGlobalRootCA.crt.pem'}})
 
 
