68eaeac11ded40aeaa122df3151d79f669168b7d
Alonso Bonilla <luis.labp06@gmail.com>
2023-05-26 20:03:39 -0500

    Corrigiendo errores

--- controllers/cliente.py
+++ controllers/cliente.py
@@ -12,16 +12,16 @@ def home():
     return render_template("client/index.html")
 #     # , simples = simples, mixtas = mixtas, alopobres = alopobres, especiales = especiales)
 
-@cliente.route("/<tipo>")
-def verMas(tipo):
-    productos = []
-    if tipo == "simples":
-        productos = Producto.getProductosTipo(1)
-    elif tipo == "mixtas":
-        productos = Producto.getProductosTipo(2)
-    elif tipo == "alopobre":
-        productos = Producto.getProductosTipo(3)
-    elif tipo == "especiales":
-        productos = Producto.getProductosTipo(4)
+# @cliente.route("/<tipo>")
+# def verMas(tipo):
+#     productos = []
+#     if tipo == "simples":
+#         productos = Producto.getProductosTipo(1)
+#     elif tipo == "mixtas":
+#         productos = Producto.getProductosTipo(2)
+#     elif tipo == "alopobre":
+#         productos = Producto.getProductosTipo(3)
+#     elif tipo == "especiales":
+#         productos = Producto.getProductosTipo(4)
     
-    return render_template("client/verMas.html", productos = productos)
+#     return render_template("client/verMas.html", productos = productos)
