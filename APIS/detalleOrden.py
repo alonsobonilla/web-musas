bac34e146531930efc8df7923a629a717e1909fd
Yampier <yampiervb12@gmail.com>
2023-06-02 17:10:34 -0500

    detalle orden obtener id

--- APIS/detalleOrden.py
+++ APIS/detalleOrden.py
@@ -43,16 +43,19 @@ def api_guardardetalleOrden():
 def api_obtenedetalleorden(idproducto,idpedido):
     try:
         listaserializable = []
+        deO = DetalleOrden.obtener_detalleOrden_id(idproducto,idpedido)
         validar_idPedido = Pedido.validar_idPedido_existente(idpedido)
         validar_idProducto = Producto.obtener_producto_por_id(idproducto)
+        
 
         if validar_idProducto is None:
             return jsonify({"Mensaje": "El producto no existe"})
         elif not validar_idPedido:
             return jsonify({"Mensaje": "El pedido no existe"})
+        elif deO is None:
+            return jsonify({"Mensaje": "El detalle de orden no existe"})
         else:
             listaserializable = []
-            deO = DetalleOrden.obtener_detalleOrden_id(idproducto,idpedido)
             miobj = DetalleOrden(deO[0],deO[1],deO[2],deO[3],deO[4],deO[5])
             listaserializable.append(miobj.midic.copy())
             return jsonify(listaserializable)
