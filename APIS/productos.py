c0dc0d183a214ab9989fe01db6767ae6acac7a6d
Yampier <yampiervb12@gmail.com>
2023-06-25 16:46:08 -0500

    apiproducto

--- APIS/productos.py
+++ APIS/productos.py
@@ -89,11 +89,13 @@ def actualizar_producto():
 
 @api_productos.route("/eliminar_producto", methods=["POST"])
 
-def eliminar_producto(id):
+def eliminar_producto():
     try:
-        producto = Producto.obtener_producto_por_id(id)
+        
+        idproducto = request.json["idProducto"]
+        producto = Producto.obtener_producto_por_id(idproducto)
         if producto is not None:
-            Producto.eliminar_producto(id)
+            Producto.eliminar_producto(idproducto)
             return jsonify({"Mensaje":"Producto eliminado correctamente", "status:":"1"})
         return jsonify({"Mensaje":"No existe el producto", "status:":"0"})
     except Exception as ex:
