80e3ba4e7ce67ff156481b1c0410d9a634b69173
Alonso Bonilla <luis.labp06@gmail.com>
2023-06-24 23:31:01 -0500

    Agregando documentacion

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_jwt import JWT
+from flask_swagger_ui import get_swaggerui_blueprint
 from Token.usuario import authenticate, identity
 from controllers.admin import *
 from controllers.cliente import *
@@ -37,6 +38,26 @@ jwt = JWT(app, authenticate, identity)
 #         return jsonify({"mensaje": "Falta username o password"})
 #     return jsonify({"mensaje": "Credenciales incorrectas"})
 
+#swagger
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/documentacion.yaml'  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
 
 admin.register_blueprint(productos)
 admin.register_blueprint(categoria_producto)
@@ -46,7 +67,8 @@ cliente.register_blueprint(auth)
 
 app.register_blueprint(admin)
 app.register_blueprint(cliente)
-
+#Registro de swagger
+app.register_blueprint(swaggerui_blueprint)
 #Registrando apis
 app.register_blueprint(api_productos)
 app.register_blueprint(api_usuarios)
